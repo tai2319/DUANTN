1c4f0f95b419c7949151e4d91a33f7b2822c1e41
tai2319 <99863150+tai2319@users.noreply.github.com>
2023-11-16 12:58:56 +0700

    Revert "Revert "tai""
    This reverts commit a0e1e7c046da1bbf553a6881002673933278d665.

--- DoAnTotNghiep/DATN/src/main/java/com/ps20652/DATN/controller/ShoppingCartController.java
+++ DoAnTotNghiep/DATN/src/main/java/com/ps20652/DATN/controller/ShoppingCartController.java
@@ -1,15 +1,15 @@
-//package com.ps20652.DATN.controller;
+// package com.ps20652.DATN.controller;
 //
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 //
-//@Controller
-//@RequestMapping("cart")
-//public class ShoppingCartController {
-//	
-//	@GetMapping("view")
-//	public String view() {
-//		return "cart/view";
-//	}
-//}
+// @Controller
+// @RequestMapping("cart")
+// public class ShoppingCartController {
+//
+// @GetMapping("view")
+// public String view() {
+// return "cart/view";
+// }
+// }
--- DoAnTotNghiep/DATN/src/main/java/com/ps20652/DATN/service/ProductService.java
+++ DoAnTotNghiep/DATN/src/main/java/com/ps20652/DATN/service/ProductService.java
@@ -4,26 +4,6 @@ import java.util.List;
 
 import com.ps20652.DATN.entity.Product;
 
-
 public interface ProductService {
 
-	
-	
-	public List<Product> findAll();
-
-	public Product create(Product pro);
-
-	public Product findbyId(Integer id);
-	
-	public Product findbyIdPro(Product product);
-	
-	Product update(Product product);
-
-	void delete(Product id);
-	
-	List<Product> findByName(String name);
-	
-	List<Product> findByPrice(double minPrice, double maxPrice);
-	
-	List<Product> findByCategoryCategoryId(int categoryId);
 }
